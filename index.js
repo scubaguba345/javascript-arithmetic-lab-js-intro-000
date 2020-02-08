154fe2b4e83b518537bf132d98df5619343e12f2
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-02-08 02:32:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function add(a,b){
    return a / b
  }
 function inc(n){
-  return n += 1;
+  return n ++ 1;
 }
 function dec(n) {
-  return n -= 1;
+  return n -- 1;
 }
 function makeInt(n){
   return parseInt(n,10);
